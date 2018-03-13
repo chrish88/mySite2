144d3c4255d6492058d67ebc56ccbcb1461b425a
chrish88 <crehall88@gmail.com>
2018-03-13 14:51:01 -0700

    added jquery smooth scroll, continued with layout styling

--- js/scripts.js
+++ js/scripts.js
@@ -25,11 +25,23 @@ function stickyNavBar() {
 
 window.addEventListener('scroll', stickyNavBar);
 
+//--------------smooth scroll------------------//
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
 
+        var target = this.hash;
+        var $target = $(target);
 
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+});
 
-
-
+//--------------Portfolio Image Display------------------//
 // capturs photo genre ul
 const ul = document.querySelector('.photoGenreList');
 //stores ul children, in an array named genreList
@@ -53,7 +65,7 @@ const longExposure = document.querySelector('#longExposure');
 
 
 
-
+//Listens for a click on genre list and displays matching genre thumbnail
 ul.addEventListener('click', (e) => {
     e.preventDefault();
     //captures the element that triggers event and puts it in var click
@@ -62,25 +74,25 @@ ul.addEventListener('click', (e) => {
     for (let i = 0; i < genreList.length; i++) {
         if (click.textContent === genreList[i].textContent) {
 
-            thumbnailChildren[i].style.display = 'block';
+            thumbnailChildren[i].style.display = 'grid';
         } else {
             thumbnailChildren[i].style.display = 'none';
         }
     }
 });
 
-
+//listens for a click on image in thumbnail and displays that image as main image
 thumbnailWrap.addEventListener('click', (e) => {
     e.preventDefault();
-    //camputres the element that triggers event and puts it in var click 
+    //caputres the element that triggers event and puts it in var click 
     const click = e.target;
     //var clickCopy is defined and the value is set to click event object
     const clickCopy = click;
-    console.log(click.className);
-    console.log(clickCopy);
-    if (click) {
+
+    if (click.className === 'galleryItem img') {
         img.src = click.src;
-        img.className = 'main-img';
+        // img.className = 'main-img';
+        img.setAttribute('border', '2px solid black');
     } else {
         mainImage.style.display = "none";
     }
