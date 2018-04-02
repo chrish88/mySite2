688c4a429207a5094828a913e88b1518505dc712
chris <crehall88@gmail.com>
2018-04-01 20:23:31 -0700

    finished the mobile layout for the flickr photo section and some of the form section

--- js/devProjects.js
+++ js/devProjects.js
@@ -4,7 +4,7 @@ labels.hide();
 
 ////////when user clicks input feild switch placeholder to label/////////
 //slect input feild and put hover event listener on it
-$('input').on( "click", function(e){  //mouseover function
+$('.login-input').on( "click", function(e){  //mouseover function
     e.preventDefault();
     let $targetIn = $(e.target);
 
@@ -16,34 +16,19 @@ $('input').on( "click", function(e){  //mouseover function
         //slide place holder text up
         $($targetIn).attr("placeholder", "");
     }
-// }, function(e){ //mouseout function
-//     e.preventDefault();
-//     //selects all input elements inside of the second form feild
-//     var secondFormInputs = $(".second-feild").children("input");
-//     //loop through secondFormInputs
-//     secondFormInputs.each(function(index){
-//         console.log(secondFormInputs + index);
-//         //var placeHolder holds the placeholder value for "this"
-//     let placeHolder = $(secondFormInputs[index]).attr("placeholder");
-//         //holds event target for mouse out function
-//         let $targetOut = $(e.target);
-//         $targetOut.prev().slideUp(400);
-//         $($targetOut).attr("placeholder", placeHolder);
-//         console.log(pla);
-//     });//end of each function
 
 });//end of mouseout function
 
 
 //***************flickr photo**************//
 
-$('#flickr-submit').click(function(e){
+$('#flickr-form').submit(function(e){
     e.preventDefault();
     const flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     let $searchInput = $('#flickr-search').val();
     let searchData = {
         tags: $searchInput,
-        format: 'json'
+        format: "json"
     };
 
     function displayPhotos(data){
@@ -51,13 +36,11 @@ $('#flickr-submit').click(function(e){
         $.each(data.items, function(i, photo){
             photoHTML += '<li class="flickr-thumbnail">';
             photoHTML += '<a href="' + photo.link + '">';
-            photoHTML += '<img href="' + photo.media.m + '"></a></li>'
+            photoHTML += '<img src="' + photo.media.m + '"></a></li>'
         });
         photoHTML += '</ul>';
-        $('#photos').html(photoHTML);
+        $('.bottom').html(photoHTML);
     }
-    $.getJSON(flickrAPI, $searchInput, searchData);
-
-    // $.getJSON(flickrAPI, data, );
+    $.getJSON(flickrAPI, searchData, displayPhotos);
 });
 //***************WEATHER WIDGET**************//
