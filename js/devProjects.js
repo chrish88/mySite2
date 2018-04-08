664f7128badeae6d4443b5023076e42b476594ea
chris <crehall88@gmail.com>
2018-04-07 21:09:24 -0700

    started working on weather widget

--- js/devProjects.js
+++ js/devProjects.js
@@ -44,3 +44,40 @@ $('#flickr-form').submit(function(e){
     $.getJSON(flickrAPI, searchData, displayPhotos);
 });
 //***************WEATHER WIDGET**************//
+
+const wundergroundAPI = "http://api.wunderground.com/api/6f1e96e055f4b933/conditions/q/36.320,-115.667.json";
+
+let weatherXhr = new XMLHttpRequest();
+
+weatherXhr.onreadystatechange = function (){
+    if(this.readyState === 4 && this.status === 200){
+        let weatherResponse = JSON.parse(weatherXhr.responseText);
+        console.log(weatherResponse);
+
+        //API data into variables
+        let location = weatherResponse.current_observation.display_location.full;
+        let timeAndDate = weatherResponse.current_observation.observation_time;
+        let temp = weatherResponse.current_observation.temperature_string;
+        let wind = weatherResponse.current_observation.wind_mph;
+        let windDirec = weatherResponse.current_observation.wind_dir;
+        let precip = weatherResponse.current_observation.precip_1hr_in;
+        let humid = weatherResponse.current_observation.relative_humidity;
+        
+        //seclectin DOM elements and injecting data
+        document.getElementById('timeDate').textContent = timeAndDate;
+        document.getElementById('temp-span').textContent = temp;
+        document.getElementById('wind-span').textContent = wind + ' mph';
+        document.getElementById('wind-direc').textContent = windDirec;
+        document.getElementById('precip-span').textContent = precip;
+        document.getElementById('humid-span').textContent = humid;
+
+    }
+}
+weatherXhr.open("GET", wundergroundAPI, true);
+weatherXhr.send();
+
+
+
+
+
+
